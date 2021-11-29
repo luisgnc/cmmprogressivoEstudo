215cc3814314f69b826040f1ac7b27b82c92544b
Luís Gonçalves <76257239+luisgnc@users.noreply.github.com>
2021-11-29 23:14:05 +0000

    Cout <<

---
+++ ProjetosCpp/CursoProgressivo2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Output:  Curso de C++ Progressivo
+    cout << "Curso de " << "C++ Progressivo" << endl;
+
+    // Ou ainda:
+    // Output: Curso de C++ Progressivo
+    cout << "Curso de ";
+    cout << "C++ Progressivo";
+
+    // Cuz int main function expects a return value.
+    return 0;
+}
