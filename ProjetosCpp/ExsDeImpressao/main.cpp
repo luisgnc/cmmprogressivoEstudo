f0e29b9dfac65738bd229e8aa1f7119f11386160
Luís Gonçalves <76257239+luisgnc@users.noreply.github.com>
2021-11-29 23:23:09 +0000

    Xmas Tree

---
+++ ProjetosCpp/ExsDeImpressao/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << endl << endl << endl;
+    cout << "       x\n      xxx\n     xxxxx\n    xxxxxxx\n   xxxxxxxxx\n  xxxxxxxxxxx" << endl;
+    cout << "       x\n       x\n     xxxxx";
+    cout << endl << endl << endl;
+    return 0;
+}
